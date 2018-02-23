8e9c647dbad626c42bf409b7e4443ba4432d99bb
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-02-23 17:37:51 -0400

    Adding rds general functions

--- src/atmos_rdb_kernel/core.clj
+++ src/atmos_rdb_kernel/core.clj
@@ -1,2 +1,18 @@
-(ns atmos-rdb-kernel.core)
+(ns atmos-rdb-kernel.core
+  (:require [korma.db :as sql]))
+
+;------------------------------
+; BEGIN General functions
+;------------------------------
+
+(defn defpersistence [db-map]
+  (let [db-map (clojure.core/update db-map
+                                    :classname #(str "com.mysql.cj.jdbc.Driver" %))]
+    (sql/mysql db-map)))
+
+(defn init-persistence [db-definition] (sql/defdb atmos-users db-definition))
+
+;------------------------------
+; END - General functions
+;------------------------------
 
