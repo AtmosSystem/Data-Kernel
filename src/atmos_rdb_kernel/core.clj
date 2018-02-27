8af1fb784f9a8b0be3156d7d895d3e38cf3ee997
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-02-27 14:36:00 -0400

    Adding crud functionalities

--- src/atmos_rdb_kernel/core.clj
+++ src/atmos_rdb_kernel/core.clj
@@ -1,5 +1,6 @@
 (ns atmos-rdb-kernel.core
-  (:require [korma.db :as sql]))
+  (:require [korma.db :as sql]
+            [korma.core :refer :all]))
 
 ;------------------------------
 ; BEGIN General functions
@@ -13,6 +14,73 @@
 (defn init-persistence [db-definition] (sql/defdb atmos-users db-definition))
 
 ;------------------------------
-; END - General functions
+; END General functions
 ;------------------------------
 
+
+;------------------------------
+; BEGIN CRUD functions
+;------------------------------
+
+(defn- fn-entity-symbol
+  ([type entity no-asterisk]
+   (symbol (apply str [(name type) "-" (name entity) (if-not no-asterisk "*")])))
+  ([type entity]
+   (fn-entity-symbol type entity false)))
+
+(defmacro defadd-entity
+  [entity args-fn add-persist-fn]
+  (let [fn-name (fn-entity-symbol :add entity)]
+    `(defn- ~fn-name
+       ~args-fn
+       (if-let [key-inserted# (~add-persist-fn (first ~args-fn))]
+         (:generated_key key-inserted#)
+         false))))
+
+(defmacro defget-identity-entity
+  [entity args-fn get-persist-fn id-name]
+  (let [fn-name (fn-entity-symbol :get entity)]
+    `(defn- ~fn-name
+       ~args-fn
+       (first (~get-persist-fn {~id-name (first ~args-fn)})))))
+
+
+(defmacro defget-all-entity
+  [entity get-persist-base-fn]
+  (let [fn-name (fn-entity-symbol :get-all entity true)]
+    `(defn ~fn-name
+       []
+       (-> ~get-persist-base-fn
+           select))))
+
+(defmacro defupdate-entity
+  [entity args-fn update-persist-fn get-entity-fn]
+  (let [fn-name (fn-entity-symbol :update entity)
+        arg-fn (first args-fn)]
+    `(defn- ~fn-name
+       ~args-fn
+       (if-let [exists# (~get-entity-fn (:id ~arg-fn))]
+         (do
+           (~update-persist-fn ~arg-fn)
+           true)
+         false))))
+
+(defn remove-cond-fn
+  [remove-entity-fn get-entity-fn cond data]
+  (if-let [entity (get-entity-fn data)]
+    (do
+      (remove-entity-fn cond)
+      true)
+    false))
+
+(defmacro defremove-entity
+  [entity args-fn remove-persist-fn get-entity-fn]
+  (let [fn-name (fn-entity-symbol :remove entity)
+        arg-fn (first args-fn)]
+    `(defn- ~fn-name
+       ~args-fn
+       (remove-cond-fn ~remove-persist-fn ~get-entity-fn {:id ~arg-fn} ~arg-fn))))
+
+;------------------------------
+; END CRUD functions
+;------------------------------
