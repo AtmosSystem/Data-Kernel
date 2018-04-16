028ddc1c75fb5f4d32aa914dafadf1078b9977b3
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-16 17:45:22 -0400

    Refactoring
    #1 | Fixing issue

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject atmos-data-kernel "0.3.0-SNAPSHOT"
-  :description "Relational database core functionality of Atmos System"
+  :description "Data core functionality of Atmos System"
   :url "https://github.com/AtmosSystem/RDB-Kernel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;persistence-deps
                  [korma "0.4.3"]
-                 [mysql/mysql-connector-java "6.0.6"]]
+                 [mysql/mysql-connector-java "8.0.8-dmr"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
