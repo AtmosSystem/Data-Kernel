5c6b43124034c0e9ce09ecb421b686016cfc6632
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-19 14:09:19 -0400

    #11 | Fixing issue

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atmos-data-kernel "0.5.1"
+(defproject atmos-data-kernel "0.5.2"
   :description "Data core functionality of Atmos System"
   :url "https://github.com/AtmosSystem/Data-Kernel"
   :license {:name "Eclipse Public License"
