f6c06ea8f3151a9f4bc10950d0c2dfc340e71086
Santiago de Pedro <santiago.depedro@numerade.com>
2022-12-01 22:16:41 -0400

    Dependencies
    This commit update all dependencies of the project.
    
    #20

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Data core functionality of Atmos System"
   :url "https://github.com/AtmosSystem/Data-Kernel"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  ;persistence-deps
                  [korma "0.4.3"]
-                 [mysql/mysql-connector-java "8.0.12"]]
+                 [mysql/mysql-connector-java "8.0.30"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
