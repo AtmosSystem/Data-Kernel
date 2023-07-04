01de14c9e34c931c7cbad97cbc2021e8ae98a333
Santiago de Pedro <santiago.pedro23@gmail.com>
2023-07-04 12:25:27 -0400

    CI
    Adding GitHub Action to project

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Data-Kernel
 
+[![Clojars Project](https://img.shields.io/clojars/v/org.clojars.atmos-system/atmos-data-kernel.svg)](https://clojars.org/org.clojars.atmos-system/atmos-data-kernel)
+[![Downloads](https://versions.deps.co/AtmosSystem/Data-Kernel/downloads.svg)](https://versions.deps.co/AtmosSystem/Data-Kernel)
 [![Dependencies Status](https://versions.deps.co/AtmosSystem/Data-Kernel/status.svg)](https://versions.deps.co/AtmosSystem/Data-Kernel)
 
 This library is the data core of atmos systems, it has commons functionality.
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject atmos-system/atmos-data-kernel "1.0-SNAPSHOT"
+(defproject org.clojars.atmos-system/atmos-data-kernel "1.0-SNAPSHOT"
   :description "Data core functionality of Atmos System"
   :url "https://github.com/AtmosSystem/Data-Kernel"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
-  :repositories [["github" {:url      "https://maven.pkg.github.com/AtmosSystem/Data-Kernel"
-                            :username :env/github_username
-                            :password :env/github_token}]])
+  :deploy-repositories [["clojars" {:url      "https://repo.clojars.org/"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]])
